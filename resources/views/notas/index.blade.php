b29e506a4cc859e69e36afef5ef1f0780f8083cd
oswaldox199 <oswaldox199@gmail.com>
2022-06-19 19:30:43 -0500

    se incluye boton de eliminar, y se cambia estructura de otros botones

--- resources/views/notas/index.blade.php
+++ resources/views/notas/index.blade.php
@@ -4,9 +4,11 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="">
-                        <button type="button" class="border text-white border-indigo-500 bg-indigo-500 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline">
-                            <a href="{{ url("/notas/create");}}">Crear</a>
-                        </button>
+                        <a href="{{ route('notas.create') }}">
+                            <button type="button" class="border text-white border-indigo-500 bg-indigo-500 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline">
+                                Crear
+                            </button>
+                        </a>
                     </div>
                     <table id="notas" class="display" style="width:100%">
                         <thead>
@@ -23,9 +25,19 @@
                                 <td>{{$nota->id}}</td>
                                 <td>{{$nota->title}}</td>
                                 <td>{{$nota->content}}</td>
-                                <td>
+                                <td class="flex">
                                     <button type="button" class="border text-white border-indigo-500 bg-indigo-500 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline">Editar</button>
-                                    <button type="button" class="border text-white border-red-500 bg-red-500 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-red-600 focus:outline-none focus:shadow-outline">Eliminar</button>
+                                    <form method="POST" action="{{ route('notas.destroy', $nota->id) }}">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="border text-white border-red-500 bg-red-500 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-red-600 focus:outline-none focus:shadow-outline">Eliminar</button>
+                                    </form>
+                                    
+                                    <a href="{{ route('notas.show',$nota->id) }}">
+                                        <button type="button" class="border border-teal-500 bg-teal-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-teal-600 focus:outline-none focus:shadow-outline">
+                                            Ver
+                                        </button>
+                                    </a>
                                 </td>
                             </tr>
                             @endforeach
